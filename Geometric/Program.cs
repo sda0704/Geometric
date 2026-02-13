ede735705e9728c915cccc4a35a6b2c685e74c1f
123 <mcoffiano@gmail.com>
2026-02-13 11:55:23 +0300

    Добавьте файлы проекта.

---
+++ Geometric/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometric
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Rectangle r = new Rectangle(10, 10, 2, 3);
+            Console.WriteLine("==Прямоугольник==");
+            Console.WriteLine($"Площадь прямоугольника: {r.Area}");
+            Console.WriteLine($"Периметр прямоугольника: {r.Perimeter}");
+            Console.WriteLine($"Координата точки X: {r.X}");
+            Console.WriteLine($"Координата точки Y: {r.Y}\n");
+
+            Console.WriteLine("==Изменение значений==");
+            r.Width = 16;
+            r.Height = 20;
+            r.X = -5;
+            r.Y = 0;
+
+
+            Console.WriteLine("==Прямоугольник==");
+            Console.WriteLine($"Площадь прямоугольника: {r.Area}");
+            Console.WriteLine($"Периметр прямоугольника: {r.Perimeter}");
+            Console.WriteLine($"Координата точки X: {r.X}");
+            Console.WriteLine($"Координата точки Y: {r.Y} \n");
+
+
+            Console.WriteLine("==Использование нескольких прямоугольников==");
+            var rectangles = new[]
+            {
+                new Rectangle(0,5,2,3),
+                new Rectangle(1,15,9,5),
+                new Rectangle(7, 6, -5,10)
+            };
+
+            foreach (var rect in rectangles)
+            {
+                Console.WriteLine(rect.ToString());
+            }
+        }
+    }
+}
---
+++ Geometric/Rectangle.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometric
+{
+    internal class Rectangle
+    {
+        private double _x;
+
+        public double X
+        {
+            get { return _x; }
+            set { _x = value; }
+        }
+
+
+        private double _y;
+
+        public double Y
+        {
+            get { return _y; }
+            set { _y = value; }
+        }
+
+
+
+        private double _width;
+        private double _height;
+
+
+        public Rectangle(double width, double height, double x, double y)
+        {
+            Width = width;
+            Height = height;
+            X = x;
+            Y = y;
+        }
+        public double Height
+        {
+            get => _height;
+            set
+            {
+                if(value < 0)
+                {
+                    throw new ArgumentException("Ошибка! Высота не может быть отрицательной");
+                }
+                _height = value;
+            }
+        }
+        public double Width
+        {
+            get => _width;
+            set
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentException("Ошибка! Ширина не может быть отрицательной");     
+                }
+                _width = value;
+            }
+        }
+        public double Area => Height * Width;
+        public double Perimeter => (Height + Width) * 2;
+
+        public override string ToString()
+        {
+            return
+                $"Площадь: {Area} " +
+                $"Периметр: {Perimeter} " +
+                $"Координата точки X: {X} " +
+                $"Координата точки Y: {Y} " ;
+            
+        }
+    }
+
+
+}
